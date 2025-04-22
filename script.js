9aaf99a46632ea180fe6d845b70e7ebcde386736
Abhiram <abhikollepara333@gmail.com>
2025-04-22 13:10:23 +0530

    Done

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+
+const jwtSecret = 'your_jwt_secret';
+const encryptionKey = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+
 const encrypt = (payload) => {
-  // encrypt the payload and return token
-}
+  const token = jwt.sign(payload, jwtSecret, { expiresIn: '1h' });
+  const cipher = crypto.createCipheriv('aes-256-cbc', encryptionKey, iv);
+  let encrypted = cipher.update(token, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+  return `${iv.toString('hex')}:${encrypted}`;
+};
 
 const decrypt = (token) => {
-  // return decoded payload
-}
+  const [ivHex, encryptedData] = token.split(':');
+  const ivBuffer = Buffer.from(ivHex, 'hex');
+  const decipher = crypto.createDecipheriv('aes-256-cbc', encryptionKey, ivBuffer);
+  let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+  return jwt.verify(decrypted, jwtSecret);
+};
 
 module.exports = {
   encrypt,
   decrypt
-}
+};
